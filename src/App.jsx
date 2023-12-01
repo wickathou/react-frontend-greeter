53060b217ffe103f8b28780515d3d2edc50acda4
Javier <javidx@gmail.com>
2023-12-01 15:38:02 +0200

    Added React router and route to greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.scss';
 import Header from './components/Header';
 import Main from './components/Main';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <RouterProvider router={router} />
     </div>
   );
 }
